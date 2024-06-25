77c24655760e3799b1e76fe10bd2e3eaf8ce2a80
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-25 14:05:11 +0700

    upgrade kernel to latest

--- nixos/boot/default.nix
+++ nixos/boot/default.nix
@@ -1,8 +1,9 @@
-{
+{ pkgs, ... }: {
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
