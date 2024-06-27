40227a38e9898424cda04a5d916685692c545826
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-27 11:36:45 +0700

    add urban-cli derivation

---
+++ my-packages/urban-cli/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
+
+buildGoModule rec {
+  pname = "urban-cli";
+  version = "0.2.4";
+
+  src = fetchFromGitHub {
+    owner = "tfkhdyt";
+    repo = "urban-cli";
+    rev = "v${version}";
+    hash = "sha256-URTEhtOiwb3IDyjRUtUmVTaeDXw4Beg0woWdGxeq098=";
+  };
+
+  vendorHash = "sha256-fEZzX+ecSWKITXczcwm5BGw5OWuixa4XKrEx8z0pxXQ=";
+
+  ldflags = [ "-s" "-w" ];
+
+  meta = with lib; {
+    description = "Blazingly fast command line interface for Urban Dictionary";
+    homepage = "https://github.com/tfkhdyt/urban-cli";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ ];
+    mainProgram = "urban-cli";
+  };
+}
