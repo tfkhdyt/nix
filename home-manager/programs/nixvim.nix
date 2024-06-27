f7ee51f242e6bc6483a4f326dbb1d6266d75194c
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-27 15:53:01 +0700

    nixvim: install lualine

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -1,4 +1,13 @@
 {
   enable = true;
   defaultEditor = true;
+  clipboard.providers.wl-copy.enable = true;
+  colorschemes.gruvbox.enable = true;
+  plugins = {
+    lualine = {
+      enable = true;
+      globalstatus = true;
+      iconsEnabled = true;
+    };
+  };
 }
