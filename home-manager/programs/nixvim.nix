1e6511b13c13bb729fec93740fc2c87788ae346a
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-27 16:28:41 +0700

    nixvim: enable lsp and add more options

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -3,6 +3,24 @@
   defaultEditor = true;
   clipboard.providers.wl-copy.enable = true;
   colorschemes.gruvbox.enable = true;
+  diagnostics = {
+    virtual_lines = {
+      only_current_line = true;
+    };
+    virtual_text = true;
+  };
+  globals = {
+    mapleader = " ";
+  };
+  keymaps = [
+     {
+      action = "<cmd>NvimTreeToggle<CR>";
+      key = "<Leader>e";
+      options = {
+        silent = true;
+      };
+    }
+  ];
   plugins = {
     lualine = {
       enable = true;
@@ -14,9 +32,13 @@
       autoReloadOnWrite = true;
       disableNetrw = true;
       hijackCursor = true;
+      diagnostics.enable = true;
+      filesystemWatchers.enable = true;
+      git.enable = true;
     };
     surround.enable = true;
     lsp = {
+      enable = true;
       servers = {
         gopls.enable = true;
         nil-ls.enable = true;
