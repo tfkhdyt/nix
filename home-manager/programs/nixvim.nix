d96711adb8dc005e276ec09df5ac3e866a5579fc
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-28 13:15:06 +0700

    nixvim: set foldcolumn to 0

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -154,7 +154,7 @@
       tabstop = 2;
       relativenumber = true;
       number = true;
-      foldcolumn = "1";
+      foldcolumn = "0";
       foldlevel = 99;
       foldlevelstart = 99;
       foldenable = true;
