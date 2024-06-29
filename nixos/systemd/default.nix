467b8226a37963d02983eca0e95f132da47d055c
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-29 11:10:27 +0700

    battery-threshold: remove bash path

--- nixos/systemd/default.nix
+++ nixos/systemd/default.nix
@@ -9,8 +9,8 @@
       serviceConfig = {
         Type = "oneshot";
         ExecStart = [
-          "/bin/bash -c 'echo 40 | sudo tee /sys/class/power_supply/BAT*/charge_start_threshold'"
-          "/bin/bash -c 'echo 50 | sudo tee /sys/class/power_supply/BAT*/charge_stop_threshold'"
+          "bash -c 'echo 40 | sudo tee /sys/class/power_supply/BAT*/charge_start_threshold'"
+          "bash -c 'echo 50 | sudo tee /sys/class/power_supply/BAT*/charge_stop_threshold'"
         ];
         RemainAfterExit = true;
       };
