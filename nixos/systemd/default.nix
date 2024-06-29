f95e030c4ad0f2dc065a1ea368a81b2b3cce6b17
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-29 11:16:48 +0700

    battery-threshold: remove sudo

--- nixos/systemd/default.nix
+++ nixos/systemd/default.nix
@@ -9,8 +9,8 @@
       serviceConfig = {
         Type = "oneshot";
         ExecStart = [
-          "bash -c 'echo 40 | sudo tee /sys/class/power_supply/BAT*/charge_start_threshold'"
-          "bash -c 'echo 50 | sudo tee /sys/class/power_supply/BAT*/charge_stop_threshold'"
+          "bash -c 'echo 40 | tee /sys/class/power_supply/BAT*/charge_start_threshold'"
+          "bash -c 'echo 50 | tee /sys/class/power_supply/BAT*/charge_stop_threshold'"
         ];
         RemainAfterExit = true;
       };
