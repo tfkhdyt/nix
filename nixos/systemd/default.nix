dc11790deda962dd7a26a08097226e8234abff1c
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-29 11:07:31 +0700

    systemd: add battery-threshold service

--- nixos/systemd/default.nix
+++ nixos/systemd/default.nix
@@ -3,5 +3,18 @@
   systemd.services = {
     postgresql.wantedBy = lib.mkForce [ ];
     # blueman.wantedBy = lib.mkForce [ ];
+    battery-threshold = {
+      enable = true;
+      description = "Set battery charge thresholds";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = [
+          "/bin/bash -c 'echo 40 | sudo tee /sys/class/power_supply/BAT*/charge_start_threshold'"
+          "/bin/bash -c 'echo 50 | sudo tee /sys/class/power_supply/BAT*/charge_stop_threshold'"
+        ];
+        RemainAfterExit = true;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
