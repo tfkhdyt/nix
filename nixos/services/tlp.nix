a5b99ad0873ec43778f154e7f68f8ee39917a00d
Taufik Hidayat <tfkhdyt@proton.me>
2024-06-29 20:33:36 +0700

    tlp: set CPU_ENERGY_PERF_POLICY_ON_AC to "performance"

--- nixos/services/tlp.nix
+++ nixos/services/tlp.nix
@@ -1,6 +1,7 @@
 {
   enable = true;
   settings = {
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     START_CHARGE_THRESH_BAT0 = 45;
     STOP_CHARGE_THRESH_BAT0 = 50;
     START_CHARGE_THRESH_BAT1 = 45;
