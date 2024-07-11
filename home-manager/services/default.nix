bbe123508b49d56cfc09d21b8ded6303040bce60
Taufik Hidayat <tfkhdyt@proton.me>
2024-07-12 06:30:28 +0700

    home/services: enable wlsunset

--- home-manager/services/default.nix
+++ home-manager/services/default.nix
@@ -2,6 +2,7 @@
   imports = [ ./dunst.nix ];
   services = {
     hypridle = import ./hypridle.nix;
+    wlsunset = import ./wlsunset.nix;
     # hyprpaper = import ./hyprpaper.nix;
 
     ssh-agent.enable = true;
---
+++ home-manager/services/wlsunset.nix
@@ -0,0 +1,8 @@
+{
+  enable = true;
+  latitude = -6.905977;
+  longitude = 107.613144;
+  systemdTarget = "hyprland-session.target";
+  # sunrise = "06:02";
+  # sunset = "17:47";
+}
