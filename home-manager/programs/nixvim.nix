5fa642835486322e416dbfb02476458b07962594
Taufik Hidayat <tfkhdyt@proton.me>
2024-07-15 06:55:52 +0700

    nixvim(notify): hide hover notification

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -21,11 +21,13 @@
         config = config or {}
         config.focus_id = ctx.method
         if not (result and result.contents) then
+          vim.notify('No information available')
           return
         end
         local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
         markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
         if vim.tbl_isempty(markdown_lines) then
+          -- vim.notify('No information available')
           return
         end
         return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
