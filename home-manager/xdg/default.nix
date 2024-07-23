7fdb1aa4291fc6a619ebe088f53cdd973bc6b3ec
Taufik Hidayat <tfkhdyt@proton.me>
2024-07-23 16:40:28 +0700

    xdg: install gtk portal

--- home-manager/xdg/default.nix
+++ home-manager/xdg/default.nix
@@ -2,6 +2,9 @@
 {
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
   };
 }
