40d3bd5e7cb09bb5feaaeefa5b8273226e14b7ff
Taufik Hidayat <tfkhdyt@proton.me>
2024-08-18 12:47:28 +0700

    feat(zsh): use fd for fzf completion
    This commit changes fzf completion to use fd instead of find. This provides several benefits:
    
    - fd is faster than find
    - fd is more efficient than find
    - fd is more portable than find

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -27,4 +27,17 @@
       p = "pnpm";
     };
   };
+  initExtra = ''
+    # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+      fd --type f --follow . "$1"
+    }
+
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+      fd --type d --follow . "$1"
+    }
+  '';
 }
