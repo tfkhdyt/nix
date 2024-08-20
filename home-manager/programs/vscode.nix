e4888a78c756212a4123240b7a8f8769b9456913
Taufik Hidayat <tfkhdyt@proton.me>
2024-08-20 17:07:45 +0700

    refactor(vscode): refactor vscode config to the correct one

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -4,113 +4,98 @@
     enable = true;
     package = pkgs.vscodium;
     userSettings = {
-      terminal.integrated = {
-        fontSize = 16;
-        gpuAcceleration = "on";
+      "terminal.integrated.fontSize" = 16;
+      "terminal.integrated.gpuAcceleration" = "on";
+      "editor.fontSize" = 16;
+      "editor.minimap.autohide" = true;
+      "editor.minimap.renderCharacters" = false;
+      "editor.inlayHints.fontSize" = 12;
+      "editor.inlayHints.padding" = true;
+      "editor.fontLigatures" = true;
+      "editor.wordWrap" = "on";
+      "editor.wrappingIndent" = "indent";
+      "editor.formatOnSave" = true;
+      "editor.tabSize" = 2;
+      "editor.mouseWheelScrollSensitivity" = 2;
+      "editor.cursorSmoothCaretAnimation" = "on";
+      "editor.quickSuggestions" = {
+        "strings" = "on";
       };
-      editor = {
-        fontSize = 16;
-        minimap = {
-          autohide = true;
-          renderCharacters = false;
-        };
-        inlayHints = {
-          fontSize = 12;
-          padding = true;
-        };
-        fontLigatures = true;
-        wordWrap = "on";
-        wrappingIndent = "indent";
-        formatOnSave = true;
-        tabSize = 2;
-        mouseWheelScrollSensitivity = 2;
-        cursorSmoothCaretAnimation = "on";
-        quickSuggestions.strings = "on";
-        codeActionsOnSave.source.fixAll = "always";
-        linkedEditing = true;
-        fontFamily = "'JetBrainsMono NFP', 'Droid Sans Mono', 'monospace', monospace";
+      "editor.linkedEditing" = true;
+      "editor.fontFamily" = "'JetBrainsMono NFP', 'Droid Sans Mono', 'monospace', monospace";
+      "editor.codeActionsOnSave" = {
+        "source.fixAll" = "always";
+        "source.organizeImports" = "always";
       };
-      go = {
-        inlayHints = {
-          assignVariableTypes = true;
-          constantValues = true;
-          parameterNames = true;
-          rangeVariableTypes = true;
-        };
-        alternateTools = {
-          staticcheck = "/home/tfkhdyt/.nix-profile/bin/staticcheck";
-          gopls = "/home/tfkhdyt/.nix-profile/bin/gopls";
-        };
-        lintOnSave = "off";
+      "go.inlayHints.assignVariableTypes" = true;
+      "go.inlayHints.constantValues" = true;
+      "go.inlayHints.parameterNames" = true;
+      "go.inlayHints.rangeVariableTypes" = true;
+      "go.alternateTools" = {
+        "gofumpt" = "/home/tfkhdyt/.nix-profile/bin/gofumpt";
+        "golangci-lint" = "/home/tfkhdyt/.nix-profile/bin/golangci-lint";
+        "gomodifytags" = "/home/tfkhdyt/.nix-profile/bin/gomodifytags";
+        "gopls" = "/home/tfkhdyt/.nix-profile/bin/gopls";
+        "impl" = "/home/tfkhdyt/.nix-profile/bin/impl";
+        "staticcheck" = "/home/tfkhdyt/.nix-profile/bin/staticcheck";
       };
-      typescript = {
-        preferGoToSourceDefinition = true;
-        inlayHints = {
-          functionLikeReturnTypes.enabled = true;
-          parameterNames.enabled = "all";
-          parameterTypes.enabled = true;
-          propertyDeclarationTypes.enabled = true;
-        };
-      };
-      javascript = {
-        preferGoToSourceDefinition = true;
-        inlayHints = {
-          functionLikeReturnTypes.enabled = true;
-          parameterNames.enabled = "all";
-          parameterTypes.enabled = true;
-          propertyDeclarationTypes.enabled = true;
-        };
-      };
-      eslint = {
-        format.enable = true;
-        validate = [
-          "javascript"
-          "typescript"
-          "javascriptreact"
-          "typescriptreact"
-          "astro"
-          "svelte"
-        ];
-      };
-      prettier.jsxSingleQuote = true;
-      svelte.enable-ts-plugin = true;
-      git = {
-        openRepositoryInParentFolders = "always";
-        confirmSync = false;
-        autofetch = true;
-      };
-      nix.enableLanguageServer = true;
-      diffEditor.hideUnchangedRegions.enabled = true;
+      "go.lintTool" = "golangci-lint";
+      "go.formatTool" = "gofumpt";
+      "typescript.preferGoToSourceDefinition" = true;
+      "typescript.inlayHints.functionLikeReturnTypes.enabled" = true;
+      "typescript.inlayHints.parameterNames.enabled" = "all";
+      "typescript.inlayHints.parameterTypes.enabled" = true;
+      "typescript.inlayHints.propertyDeclarationTypes.enabled" = true;
+      "javascript.preferGoToSourceDefinition" = true;
+      "javascript.inlayHints.functionLikeReturnTypes.enabled" = true;
+      "javascript.inlayHints.parameterNames.enabled" = "all";
+      "javascript.inlayHints.parameterTypes.enabled" = true;
+      "javascript.inlayHints.propertyDeclarationTypes.enabled" = true;
+      "eslint.format.enable" = true;
+      "eslint.validate" = [
+        "javascript"
+        "typescript"
+        "javascriptreact"
+        "typescriptreact"
+        "astro"
+        "svelte"
+      ];
+      "prettier.jsxSingleQuote" = true;
+      "svelte.enable-ts-plugin" = true;
+      "git.openRepositoryInParentFolders" = "always";
+      "git.confirmSync" = false;
+      "git.autofetch" = true;
+      "diffEditor.hideUnchangedRegions.enabled" = true;
       "[jsonc]" = {
-        editor.defaultFormatter = "vscode.json-language-features";
+        "editor.defaultFormatter" = "vscode.json-language-features";
       };
       "[astro]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
       "[typescriptreact]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
       "[javascript]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
       "[typescript]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
       "[svelte]" = {
-        editor.defaultFormatter = "svelte.svelte-vscode";
+        "editor.defaultFormatter" = "svelte.svelte-vscode";
       };
       "[markdown]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
-      redhat.telemetry.enabled = false;
-      search.exclude = {
+      "redhat.telemetry.enabled" = false;
+      "search.exclude" = {
         "**/bower_components" = true;
         "**/env" = true;
         "**/node_modules" = true;
         "**/pnpm-lock.yaml" = true;
         "**/venv" = true;
       };
-      files.watcherExclude = {
+      "files.watcherExclude" = {
         "**/.git/objects/**" = true;
         "**/.git/subtree-cache/**" = true;
         "**/node_modules/**" = true;
@@ -118,14 +103,22 @@
         "**/venv/**" = true;
         "env-*" = true;
       };
-      workbench = {
-        colorTheme = "Gruvbox Dark Hard";
-        iconTheme = "material-icon-theme";
-        startupEditor = "none";
-      };
-      errorLens = {
-        gutterIconsEnabled = true;
-        gutterIconSet = "defaultOutline";
+      "workbench.colorTheme" = "Gruvbox Dark Hard";
+      "workbench.iconTheme" = "material-icon-theme";
+      "workbench.startupEditor" = "none";
+      "errorLens.gutterIconsEnabled" = true;
+      "errorLens.gutterIconSet" = "defaultOutline";
+      "emeraldwalk.runonsave" = {
+        "commands" = [
+          {
+            "cmd" = "goimports-reviser -rm-unused -set-alias -format -use-cache -output write \${file}";
+            "match" = "\\.go$";
+          }
+          {
+            "cmd" = "golines \${file} -w";
+            "match" = "\\.go$";
+          }
+        ];
       };
     };
     extensions = with pkgs.vscode-marketplace; [
@@ -154,6 +147,7 @@
       yzhang.markdown-all-in-one
       yoavbls.pretty-ts-errors
       ttoowa.in-your-face-incredible
+      emeraldwalk.runonsave
     ];
   };
 }
