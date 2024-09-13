6c13b00a73893b86e8d547ef5c397deffccd3c33
Taufik Hidayat <tfkhdyt@proton.me>
2024-09-13 20:02:48 +0700

    fix(nixvim): enhance code formatter with early bail out
    Fixes the issue where code formatters always run, even when they are not needed which could lead to slow performance.

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -403,10 +403,40 @@
       conform-nvim = {
         enable = true;
         settings = {
-          format_on_save = {
-            lsp_format = "fallback";
-            timeout_ms = 3000;
-          };
+          format_on_save = # Lua
+            ''
+              function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                  return
+                end
+
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                  return
+                end
+
+                local function on_format(err)
+                  if err and err:match("timeout$") then
+                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                  end
+                end
+
+                return { timeout_ms = 200, lsp_fallback = true }, on_format
+               end
+            '';
+          format_after_save = # Lua
+            ''
+              function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                  return
+                end
+
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                  return
+                end
+
+                return { lsp_fallback = true }
+              end
+            '';
           formatters_by_ft = {
             javascript = [ "prettierd" ];
             typescript = [ "prettierd" ];
