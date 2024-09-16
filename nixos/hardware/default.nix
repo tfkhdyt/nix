be6424c3429e2ede5ba29d1277cbe6e5b57b7eac
Taufik Hidayat <tfkhdyt@proton.me>
2024-09-16 20:00:58 +0700

    feat(hardware): enable 32-bit support for Intel VAAPI driver
    Enables 32-bit support for the Intel VAAPI driver, which allows for better video playback in Firefox and Chromium.
    Also enables all firmware and redistributable firmware.

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -8,8 +8,11 @@
         intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         libvdpau-va-gl
       ];
-      # driSupport32Bit = true;
+      enable32Bit = true;
+      extraPackages32 = with pkgs.pkgsi686Linux; [ intel-vaapi-driver ];
     };
+    enableAllFirmware = true;
+    enableRedistributableFirmware = true;
     pulseaudio.enable = false;
     bluetooth.enable = true;
     bluetooth.powerOnBoot = true;
