2a125be8a47f5c29a30a12e3ff5c7b02bed6b000
Taufik Hidayat <tfkhdyt@proton.me>
2024-10-01 06:04:36 +0700

    hyprland: disable fake fullscreen

--- home-manager/wayland/default.nix
+++ home-manager/wayland/default.nix
@@ -58,7 +58,7 @@
           "$mainMod SHIFT, down, movewindow, d"
           "$mainMod, mouse_down, workspace, e+1"
           "$mainMod, mouse_up, workspace, e-1"
-          "$mainMod, F, fullscreen, 1"
+          "$mainMod, F, fullscreen"
           ", xf86audiomicmute, exec, $scriptDir/mic.sh"
           ", xf86audioraisevolume, exec, $scriptDir/volume.sh up"
           ", xf86audiolowervolume, exec, $scriptDir/volume.sh down"
