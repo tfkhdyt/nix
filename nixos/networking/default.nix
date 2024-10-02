d33842c7fc9b77ea3e67e584f44d5e1410f40c6e
Taufik Hidayat <tfkhdyt@proton.me>
2024-10-02 14:34:57 +0700

    fix: update default nameservers
    Update the default nameservers to use cloudflare and google.

--- nixos/networking/default.nix
+++ nixos/networking/default.nix
@@ -4,11 +4,11 @@
 
     hostName = "nixos"; # Define your hostname.
     nameservers = [
-      "103.87.68.194" # bebasid dns
-      "94.140.14.14" # adguard
-      "94.140.15.15"
-      "3.0.86.126" # puredns
-      "3.1.94.218"
+      # "103.87.68.194" # bebasid dns
+      # "94.140.14.14" # adguard
+      # "94.140.15.15"
+      # "3.0.86.126" # puredns
+      # "3.1.94.218"
       "1.1.1.1" # cloudflare
       "8.8.8.8" # google
     ];
