8dae5274a9979a8103f74447ece2ca1b5a77f531
Taufik Hidayat <tfkhdyt@proton.me>
2024-10-03 07:14:04 +0700

    systemd: limit battery threshold to 51

--- nixos/systemd/default.nix
+++ nixos/systemd/default.nix
@@ -12,8 +12,8 @@
       description = "Set the battery charge threshold";
       after = [ "multi-user.target" ];
       script = ''
-        bash -c 'echo 50 > /sys/class/power_supply/BAT0/charge_control_end_threshold'
-        bash -c 'echo 50 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+        bash -c 'echo 51 > /sys/class/power_supply/BAT0/charge_control_end_threshold'
+        bash -c 'echo 51 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
       '';
       unitConfig = {
         Type = "oneshot";
