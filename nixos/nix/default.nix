dc3edaaad79200365a82fbc7e0f82fa41e921679
Taufik Hidayat <tfkhdyt@proton.me>
2024-12-19 07:47:35 +0700

    nixos: enable auto storage optimization

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -18,6 +18,8 @@
     # Opinionated: make flake registry and nix path match flake inputs
     # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+    optimise.automatic = true;
   };
 
 }
