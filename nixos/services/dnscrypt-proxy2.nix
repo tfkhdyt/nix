c7468257e059f6eede38c7bbb6e079e6e7f1fa4a
Taufik Hidayat <tfkhdyt@proton.me>
2024-12-26 20:58:13 +0700

    disable dnscrypt

--- nixos/services/dnscrypt-proxy2.nix
+++ nixos/services/dnscrypt-proxy2.nix
@@ -1,5 +1,5 @@
 {
-  enable = true;
+  enable = false;
   settings = {
     server_names = [ "bebasdns" ];
     static.bebasdns.stamp = "sdns://AgMAAAAAAAAADTEwMy44Ny42OC4xOTYAHWludGVybmV0c2VoYXQuYmViYXNpZC5jb206NDQzCi9kbnMtcXVlcnk";
