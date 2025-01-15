45b51de0d483730fbf11855fda682170223f7636
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-15 09:58:41 +0700

    chore(systemd): disable waydroid-container service from running on boot

--- nixos/systemd/default.nix
+++ nixos/systemd/default.nix
@@ -4,8 +4,7 @@
     postgresql.wantedBy = lib.mkForce [ ];
     mysql.wantedBy = lib.mkForce [ ];
     httpd.wantedBy = lib.mkForce [ ];
-    # waydroid-container.wantedBy = lib.mkForce [ ];
-    # blueman.wantedBy = lib.mkForce [ ];
+    waydroid-container.wantedBy = lib.mkForce [ ];
     charging-threshold = {
       enable = false;
       path = with pkgs; [ bash ];
