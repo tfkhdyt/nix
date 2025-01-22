c3d4c6288861553e49d32a7d615e87ced32ba4f7
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-23 05:04:24 +0700

    switch back to supermaven

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -219,7 +219,7 @@
       christian-kohler.npm-intellisense
       christian-kohler.path-intellisense
       esbenp.prettier-vscode
-      # supermaven.supermaven
+      supermaven.supermaven
       svelte.svelte-vscode
       bradlc.vscode-tailwindcss
       meganrogge.template-string-converter
@@ -245,7 +245,6 @@
       pkief.material-icon-theme
       ziglang.vscode-zig
       mkhl.direnv
-      continue.continue
     ];
     keybindings = [
       {
