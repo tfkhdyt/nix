9b49c0a9f8bd8cc3c8cbc00e0cf270b30f09c27d
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-22 08:32:12 +0700

    install lazygit

--- home-manager/programs/lazygit.nix
+++ home-manager/programs/lazygit.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
 {
   programs.lazygit = {
-    enable = false;
-    settings = {
-      # git.paging = {
-      #   colorArg = "always";
-      #   pager = "${pkgs.delta}/bin/delta --paging=never";
-      # };
-    };
+    enable = true;
+    # settings = {
+    # git.paging = {
+    #   colorArg = "always";
+    #   pager = "${pkgs.delta}/bin/delta --paging=never";
+    # };
+    # };
   };
 }
