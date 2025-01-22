af010dd48472d61422e37a5d5fef7c585156863d
Taufik Hidayat <tfkhdyt@proton.me>
2025-01-22 09:59:31 +0700

    move XDG_DATA_DIRS to variables

--- home-manager/home/default.nix
+++ home-manager/home/default.nix
@@ -1,4 +1,3 @@
-{ lib, ... }:
 {
   imports = [
     ./packages.nix
@@ -8,10 +7,5 @@
     username = "tfkhdyt";
     homeDirectory = "/home/tfkhdyt";
     stateVersion = "24.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    sessionVariables = {
-      #   QT_QPA_PLATFORMTHEME = lib.mkForce "gtk3";
-      #   QT_STYLE_OVERRIDE = lib.mkForce "adwaita-dark";
-      XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
-    };
   };
 }
